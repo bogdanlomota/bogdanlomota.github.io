9e4530f60bc5767a07f92ad7da0ae07c248d7fd6
Bogdan <lomotabogdan12@gmail.com>
2022-12-17 15:49:45 +0200

    add favicon

--- JS/contactform.js
+++ JS/contactform.js
@@ -1,10 +1,19 @@
-function SendMail(){
+function SendMail(event){
+    event.preventDefault();
+    const formfullnameinput=document.getElementById("fullName");
+    const formemailinput=document.getElementById("email_id");
+    const formmessageinput=document.getElementById("message");
     var params = {
-        from_name : document.getElementById("fullName").value,
-        email_id : document.getElementById("email.id").value,
-        message : document.getElementById("message").value
+        from_name : formfullnameinput.value,
+        email_id : formemailinput.value,
+        message : formmessageinput.value
     }
     emailjs.send("service_wv9hmue","template_1irpg5x",params).then(function(res){
         alert("Thanks for reaching out!");
+        formfullnameinput.value="";
+        formemailinput.value="";
+        formmessageinput.value="";
     })
 }
+
+
